2cc4dbcd241d080b9658f5f3623a76f7f11f1a6a
IsgroFranco <isgro.franco1@gmail.com>
2023-04-16 19:10:13 -0300

    add hamburger menu

--- js/mani.js
+++ js/mani.js
@@ -1,11 +1,25 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const enlaces = document.querySelectorAll("#nav a");
 
-abrir.addEventListener("click", () => {
-  nav.classList.add("visible");
-});
+function toggleMenu() {
+  nav.classList.toggle("visible");
+  abrir.classList.toggle("cerrar-menu");
+  abrir.classList.toggle("abrir-menu");
+  cerrar.classList.toggle("abrir-menu");
+  cerrar.classList.toggle("cerrar-menu");
+}
 
-cerrar.addEventListener("click", () => {
-  nav.classList.remove("visible");
+abrir.addEventListener("click", toggleMenu);
+cerrar.addEventListener("click", toggleMenu);
+
+enlaces.forEach((enlace) => {
+  enlace.addEventListener("click", () => {
+    nav.classList.remove("visible");
+    abrir.classList.remove("cerrar-menu");
+    abrir.classList.add("abrir-menu");
+    cerrar.classList.remove("abrir-menu");
+    cerrar.classList.add("cerrar-menu");
+  });
 });
