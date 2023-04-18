07a8b00a7375bf90be1f604d025cfca7ae194dbc
IsgroFranco <isgro.franco1@gmail.com>
2023-04-17 22:12:33 -0300

    fixed responsive

--- js/mani.js
+++ js/mani.js
@@ -2,9 +2,11 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 const enlaces = document.querySelectorAll("#nav a");
+const links = document.querySelector("#nav ul");
 
 function toggleMenu() {
   nav.classList.toggle("visible");
+  links.classList.toggle("visible");
   abrir.classList.toggle("cerrar-menu");
   abrir.classList.toggle("abrir-menu");
   cerrar.classList.toggle("abrir-menu");
